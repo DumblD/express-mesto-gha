2b84ce60e777188db08e556f729d5e0d51e21a5b
Artyom Zinin <NicYKN@yandex.ru>
2023-06-23 01:44:09 +0300

    fix: error 400 - validation error

--- utils/errorMessageConfig.js
+++ utils/errorMessageConfig.js
@@ -3,13 +3,15 @@ const sendError = (err, res) => {
     res.status(404).send({
       message: 'По запросу ничего не найдено',
     });
-  } else if (err.message === 'ValidationError') {
+  } else if (err.message.toLowerCase().includes('validation')) {
     res.status(400).send({
       message: 'Переданы некорректные данные',
     });
   } else {
     res.status(500).send({
       message: 'Произошла ошибка',
+      errorName: err.name,
+      errorMessage: err.message,
     });
   }
 };
